618d05604726562cbb97547cdf325ee15b954baa
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2023-07-24 18:57:11 -0300

    add text output

--- scrapper/get_votes.py
+++ scrapper/get_votes.py
@@ -1,31 +1,40 @@
 import requests
 from PyPDF2 import PdfReader
 
-from pathlib import Path
-
 def download_pdf(url, file_name):
+
     headers = {
-        "User-Agent": "Chrome/51.0.2704.103",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
     }
-    response = requests.get(url, headers=headers)
-    filename = Path(file_name)
-    
-    # Save the PDF
+
+    response = requests.get(url = url, headers=headers, allow_redirects = True)
     if response.status_code == 200:
-        filename.write_bytes(response.content)
+        with open(file_name, 'wb') as f:
+            f.write(response.content)
     else:
         print(response.status_code)
 
 # definir a URL do arquivo PDF
 url = "https://rl.senado.gov.br/reports/rwservlet?legis&report=/forms/parlam/vono_r01.RDF&paramform=no&p_cod_materia_i=138100&p_cod_materia_f=138100&p_cod_sessao_votacao_i=6675&p_cod_sessao_votacao_f=6675&p_order_by=nom_parlamentar"
-download_pdf(url, "page.pdf")
+file_name = "out/page.pdf"
+download_pdf(url, file_name)
 
-# abrir o arquivo PDF a partir do conteúdo da resposta HTTP
-pdf_file = PdfReader("page.pdf")
-infos = pdf_file.documentInfo
-# percorrer todas as páginas do PDF
-for page in pdf_file.pages:
-    # extrair o conteúdo de uma página específica
-    content = page.extract_text
-    # fazer algo com o conteúdo extraído
-    print(content)
+def extrair_texto_pdf(file_name):
+    texto = ""
+    with open(file_name, "rb") as arquivo:
+        pdf_reader = PdfReader(arquivo)
+        num_paginas = len(pdf_reader.pages)
+        for pagina_num in range(num_paginas):
+            pagina = pdf_reader.pages[pagina_num]
+            texto += pagina.extract_text()
+    return texto
+
+texto_extraido = extrair_texto_pdf(file_name)
+
+with open("out/texto_pdf.txt", "w") as arquivo:
+    # Escrever o texto no arquivo
+    arquivo.write(texto_extraido)
